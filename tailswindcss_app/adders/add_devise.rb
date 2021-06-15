72a3ac7255d4374298f166cdeeec3715bf0cb921
Richard Logwood <richard.logwood.com>
2021-06-14 23:14:52 -0400

    initail version of tailwindcss app creation with devise and stimulusjs

---
+++ tailswindcss_app/adders/add_devise.rb
@@ -0,0 +1,22 @@
+
+
+def do_bundle
+  # Custom bundle command ensures dependencies are correctly installed
+  Bundler.with_unbundled_env { run "bundle install" }
+end
+
+def add_devise
+  run "bundle add devise"
+  do_bundle
+
+  rails_command "generate devise:install"
+
+  model_name = ask("What do you want to call your Devise model?")
+  attributes = ""
+  if yes?("Do you want to any extra attributes to #{model_name}? [y/n]")
+    attributes = ask("What attributes?")
+  end
+
+  # We don't use rails_command here to avoid accidentally having RAILS_ENV=development as an attribute
+  run "rails generate devise #{model_name} #{attributes}"
+end
