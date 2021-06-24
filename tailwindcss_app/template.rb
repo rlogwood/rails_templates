f151383641bb93293b24cbbe1c9a5c627ee0007f
Richard Logwood <richard.logwood.com>
2021-06-24 17:51:39 -0400

    support automatic clone from github when referencing repo from command line

--- tailwindcss_app/template.rb
+++ tailwindcss_app/template.rb
@@ -1,5 +1,31 @@
-# require every file in the adders directory
-Dir[File.join(__dir__, 'adders', '*.rb')].each { |file| require file }
+
+# Copied from: https://raw.githubusercontent.com/excid3/jumpstart/master/template.rb
+# ==================================================================
+# Copied from: https://github.com/mattbrictson/rails-template
+# Add this template directory to source_paths so that Thor actions like
+# copy_file and template resolve against our source files. If this file was
+# invoked remotely via HTTP, that means the files are not present locally.
+# In that case, use `git clone` to download them to a local temporary dir.
+def add_template_repository_to_source_path
+  if __FILE__ =~ %r{\Ahttps?://}
+    require "tmpdir"
+    source_paths.unshift(tempdir = Dir.mktmpdir("jumpstart-"))
+    at_exit { FileUtils.remove_entry(tempdir) }
+    git clone: [
+      "--quiet",
+      "https://github.com/excid3/jumpstart.git",
+      tempdir
+    ].map(&:shellescape).join(" ")
+
+    if (branch = __FILE__[%r{jumpstart/(.+)/template.rb}, 1])
+      Dir.chdir(tempdir) do
+        git checkout: branch
+      end
+    end
+  else
+    source_paths.unshift(File.dirname(__FILE__))
+  end
+end
 
 # TODO: research this, from Chris' RailsBytes for devise
 def do_bundle
@@ -32,8 +58,11 @@ def post_bundle_application_updates
   end
 end
 
-def source_paths
-  [__dir__]
+def require_template_adder_helpers
+  # require every file in the adders directory
+  Dir[File.join(__dir__, 'adders', '*.rb')].each { |file| require file }
 end
 
+add_template_repository_to_source_path
+require_template_adder_helpers
 post_bundle_application_updates
